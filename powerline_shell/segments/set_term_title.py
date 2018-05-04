e15ba441dbd3aa04c0e81376309f9a7125739948
Matthew Judy <matt@nibfile.com>
2018-05-04 14:52:01 -0400

    Disable some of the commands for now...

--- powerline_shell/segments/set_term_title.py
+++ powerline_shell/segments/set_term_title.py
@@ -8,28 +8,29 @@ class Segment(BasicSegment):
     def add_to_powerline(self):
     
         powerline         = self.powerline
+        shell             = powerline.args.shell
         term              = os.getenv('TERM')
         is_apple_terminal = ( 'Apple_Terminal' in os.getenv('TERM_PROGRAM') )
 
         if not (('xterm' in term) or ('rxvt' in term)):
             return
             
-        if powerline.args.shell == 'bash':
+        if shell == 'bash':
             set_title = '\\[\\e]0;\\u@\\h: \\w\\a\\]'
             
-        elif powerline.args.shell == 'zsh':
+        elif shell == 'zsh':
             set_title = '%{\033]0;%n@%m: %~\007%}'
             
-        elif powerline.args.shell == 'tcsh':
-        
-            window_title_text  = "%n@%m: %/"
-            set_title          = "%{\033]0;" + window_title_text + "\a%}"  # Xterm Command: Window Title
+        elif shell == 'tcsh':
             
-            if is_apple_terminal:
-                set_title += "%{\033]7;" + "%/" + "\a%}"                    # OS Command: Working Directory
-                set_title += "%{\033]2;" + window_title_text + "\a%}"       # OS Command: Window Title
-                set_title += "%{\033]1;" + "I AM TAB" + "\a%}"              # OS Command: Tab Title
+            if is_apple_terminal:   
+                set_title  = "%{\033]7;" + "%/"    + "\a%}"  # OS Command: Working Directory
+#               set_title += "%{\033]2;" + "%n@%m" + "\a%}"  # OS Command: Window Title
+#               set_title += "%{\033]1;" + "%c"    + "\a%}"  # OS Command: Tab Title
                 
+            else:
+                set_title = "%{\033]0;" + "%n@%m: %/" + "\a%}"
+
         else:
             set_title = '\033]0;%s@%s: %s\a' % (
                 os.getenv('USER'),
