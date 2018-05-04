53bfb75d008131dc3bf8f91d3718f950827ce51a
Matthew Judy <matt@nibfile.com>
2018-05-04 15:06:55 -0400

    Space for testing

--- powerline_shell/utils.py
+++ powerline_shell/utils.py
@@ -17,7 +17,7 @@ else:
 class RepoStats(object):
     symbols = {
         'detached': u'\u2693',
-        'ahead': u'\u2B06',
+        'ahead':  u'\u2B06',
         'behind': u'\u2B07',
         'staged': u'\u2714',
         'changed': u'\u270E',
